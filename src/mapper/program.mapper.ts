1736f3f01ab2890d83fbccf6f0ec7ae87454aa96
Pranav Raj KPV <kpvpranavraj2305@gmail.com>
2026-05-11 14:04:01 +0530

    fix(program): convert start and end dates to Date objects and handle null values

--- src/mapper/program.mapper.ts
+++ src/mapper/program.mapper.ts
@@ -1,23 +1,29 @@
 import { IProgram } from "../interfaces/program.interface.js";
 
-// the programme datas that shown in employee side 
+// the programme datas that shown in employee side
 export const programResponseMapper = (programs: IProgram[]) => {
    return programs.map((program) => {
-      const start = program.startDate;
-      const end = program.endDate;
+      const start = program.startDate
+         ? new Date(program.startDate)
+         : null;
+
+      const end = program.endDate
+         ? new Date(program.endDate)
+         : null;
 
       let duration: number | null = null;
 
       if (start && end) {
          const diff = end.getTime() - start.getTime();
+
          duration = Math.ceil(diff / (1000 * 60 * 60 * 24));
       }
 
       return {
          title: program.title,
          venue: program.venue,
-         start_date: program.startDate,
-         end_date: program.endDate,
+         start_date: start,
+         end_date: end,
          duration,
       };
    });
