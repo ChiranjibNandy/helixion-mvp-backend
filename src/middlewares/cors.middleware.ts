af0023f6998dd7f0d0a191d6fc68f7af8de7ecc4
Vivek Verma <567vivekverma@gmail.com>
2026-06-13 00:51:53 +0530

    fix: strip trailing slash from FRONTEND_URL in CORS check

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,7 +1,21 @@
 import cors from "cors";
 import { ENV } from "../config/env.js";
 
+const allowedOrigins = [
+  ENV.FRONTEND_URL.replace(/\/$/, ""),
+  "http://localhost:3000",
+];
+
 export const corsMiddleware = cors({
-  origin: ENV.FRONTEND_URL,
-  credentials: true
+  origin: (origin, callback) => {
+    const normalizedOrigin = origin?.replace(/\/$/, "");
+    if (!normalizedOrigin || allowedOrigins.includes(normalizedOrigin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS: origin ${origin} not allowed`));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 });
