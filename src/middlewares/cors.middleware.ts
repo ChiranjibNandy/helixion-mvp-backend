3568271336d3f6beb447d1d94027163616794374
Vivek Verma <567vivekverma@gmail.com>
2026-06-13 11:36:38 +0530

    fix: allow all origins in CORS to unblock deployment

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,20 +1,7 @@
 import cors from "cors";
-import { ENV } from "../config/env.js";
-
-const allowedOrigins = [
-  ENV.FRONTEND_URL.replace(/\/$/, ""),
-  "http://localhost:3000",
-];
 
 export const corsMiddleware = cors({
-  origin: (origin, callback) => {
-    const normalizedOrigin = origin?.replace(/\/$/, "");
-    if (!normalizedOrigin || allowedOrigins.includes(normalizedOrigin)) {
-      callback(null, true);
-    } else {
-      callback(new Error(`CORS: origin ${origin} not allowed`));
-    }
-  },
+  origin: true,
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
